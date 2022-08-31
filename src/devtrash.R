e462e9ea7d80e398fdf4d5b1e9a92a23043cbcfd
Sebastian Castillo <castilloclaudiosebastian@gmail.com>
2022-08-31 12:37:51 -0300

    avance en TS

---
+++ src/devtrash.R
@@ -0,0 +1,13 @@
+dataset = fread(paste0(PROCESSED_DATA_DIR, "/dataset_growth4.csv.gz"))
+exp_nombre = "001TSa"
+#leo los parametros con los que se llamo al script  R
+if( is.na(exp_nombre ) ) {
+  args  <- commandArgs( trailingOnly= TRUE )
+}  else {
+  args  <- c( exp_nombre )
+}
+#el primer parametro tiene el nombre del  yml
+EXP  <- yaml::read_yaml( paste0(EXP_DIR,"/", args[1], ".yml") )
+EXP$experiment$name  <- args[1]
+PARAM  <- EXP$param
+  
