c7ca73d70787533f98ab882c83ed52d874aa4d56
Sebastian Castillo <castilloclaudiosebastian@gmail.com>
2022-09-27 06:27:36 -0300

    ya en hiperparameter tuning

--- src/completo/predicciones4.R
+++ src/completo/predicciones4.R
@@ -91,6 +91,65 @@ plan(strategy = sequential)
 tune_results_prophet_boost_1 %>% 
   show_best("rmse", n = Inf)
 
+tune_results_prophet_boost_1 %>% 
+  show_best("rsq", n = Inf)
 
 
+# This is the most important part of the process: as a data scientist, you will decide of the tuning strategy.
+# 
+# Try to spot the plot with a monotonic smooth fonction (blue line), descending for RMSE or ascending for R-squared. Here, it corresponds to "Learning Rate (log-10)" parameter. 
+# Update the grid spec with a new range of values for Learning Rate where the RMSE is minimal.
+
+# Generally speaking we will do the following steps for each tuning round
+# 
+# 1.update or adjust the parameter range within the grid specification.
+# 
+# 2.toggle on parallel processing
+# 
+# 3.perform hyperparameter tuning with new grid specification
+# 
+# 4.toggle off parallel processing
+# 
+# 5. analyze best RMSE and RSQ results
+# 
+# 6. go to 1.
+
+
+gr1<- tune_results_prophet_boost_1 %>%
+  autoplot() +
+  geom_smooth(se = FALSE)
+
+ggplotly(gr1)
+
+# Round 2
+# We fix learn_rate parameter range of values.
+
+# 1. update or adjust the parameter range within the grid specification.
+
+set.seed(123)
+grid_spec_2 <- grid_latin_hypercube(
+  extract_parameter_set_dials(model_spec_prophet_boost_tune) %>% 
+    update(mtry = mtry(range = c(1, 50)),
+           learn_rate = learn_rate(range = c(-2.0, -1.0))),
+  size = 20
+)
+
+# 2. toggle on parallel processing
+plan(
+  strategy = cluster,
+  workers  = parallel::makeCluster(n_cores)
+)
+
+
+# 3. perform hyperparameter tuning with new grid specification
+tune_results_prophet_boost_2 <- wflw_spec_prophet_boost_tune %>%
+  tune_grid(
+    resamples  = resamples_kfold,
+    grid = grid_spec_2,
+    control = control_grid(verbose = TRUE, 
+                           allow_par = TRUE)
+  )
+
+# 4. toggle off parallel processing (pendiente)
+plan(strategy = sequential)
 
